3b3b50bffd62ea33df5ff52c41f20cb7fd595e79
Awesome <2281280195@qq.com>
2019-06-14 01:01:05 -0700

    awesome123 PC ubuntu auto run script file

---
+++ user.sh
@@ -0,0 +1,9 @@
+export PATH=$PATH:/home/awesome123/Joyton/mind/Copter3.6.9-JT-Yaw/ardupilot/Tools/autotest
+
+export PATH=$PATH:/usr/lib/ccache
+
+. ~/.bashrc
+
+export PATH=$PATH:/opt/gcc-arm-none-eabi-6-2017-q2-update/bin
+
+echo $PATH
